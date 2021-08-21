d11d4e4f4f78e6f7d65e2db81e864d623012ebe1
Abimaelr <abimaelr.albu@gmail.com>
2021-08-21 20:16:11 -0300

    Seção home criada, e add temas personalizados em config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-const colors = require('tailwindcss/colors');
 
 module.exports = {
   purge: ["./public/**/*.html"],
@@ -6,17 +5,32 @@ module.exports = {
     extend: {
       backgroundImage: theme => ({
         'hero': "url('/media/home-back.png')",
+        'home': "url('/media/home1.svg')",
        })
     },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.trueGray,
-      indigo: colors.indigo,
-      red: colors.rose,
-      yellow: colors.amber,
+      black: '#000000',
+      white:'#FFF',
+      yellow: '#dff755',
+      blue: {
+        light: '#85d7ff',
+        DEFAULT: '#1fb6ff',
+        dark: '#009eeb',
+      },
+      pink: {
+        light: '#ff7ce5',
+        DEFAULT: '#ff49db',
+        dark: '#ff16d1',
+      },
+      gray: {
+        darkest: '#1f2d3d',
+        dark: '#3c4858',
+        DEFAULT: '#c0ccda',
+        light: '#e0e6ed',
+        lightest: '#f9fafc',
+      }
     },
     height: {
       screen: '100vh',
@@ -26,6 +40,8 @@ module.exports = {
       lg: '24px',
       xl: '48px',
       hero: '80%',
+      icon: '64px',
+      btn: '60px',
      },
      width: {
       screen: '100vh',
@@ -34,8 +50,14 @@ module.exports = {
       md: '16px',
       lg: '24px',
       xl: '48px',
+      
+      card: '320px',
       heroText: '350px',
       hero: '80%',
+      minContent: 'min-content',
+      maxContent: 'max-content',
+      auto: "auto",
+      btn: '200px',
      },
     backgroundSize: {
       'auto': 'auto',
