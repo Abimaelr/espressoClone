9220d8c501f49df0908647ce638e12bea9659b80
Abimaelr <abimaelr.albu@gmail.com>
2021-08-21 19:16:01 -0300

    Cover done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,50 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ["./public/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'hero': "url('/media/home-back.png')",
+       })
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+    },
+    height: {
+      screen: '100vh',
+      full: '100%',
+      sm: '8px',
+      md: '16px',
+      lg: '24px',
+      xl: '48px',
+      hero: '80%',
+     },
+     width: {
+      screen: '100vh',
+      full: '100%',
+      sm: '8px',
+      md: '16px',
+      lg: '24px',
+      xl: '48px',
+      heroText: '350px',
+      hero: '80%',
+     },
+    backgroundSize: {
+      'auto': 'auto',
+      'cover': 'cover',
+      'contain': 'contain',
+      '80%': '80%',
+      '50%': '50%',
+      '16': '4rem',
+    }
   },
   variants: {},
   plugins: [],
